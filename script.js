fa8136d96017d6482b1456d4216d46d1bd7bd0e6
Click-Clicker-2 <43284026+Click-Clicker-2@users.noreply.github.com>
2018-09-15 10:43:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //variables variables variables variables variables variables variables variables variables variables variables variables variables variables//
 var bossHP = 200; //the amount of hp the boss has
 var bossValue = 200; //the base hp of the boss
-var clicks = 20; //number of clicks the user has
+var clicks = 0; //number of clicks the user has
 var superClicks = 16; //the number of super clicks the user has (these are gained by buying them with clicks per minute)
 var clickValue = 1; //how many clicks you get everytime you click the button
 var clicksPerSecond = 0; //how many clicks you get per second automatically
